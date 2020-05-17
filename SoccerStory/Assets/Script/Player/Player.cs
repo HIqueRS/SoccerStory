f0215759629e679e58e13c778df09ad0311dc7d0
Henrique Souza de Lima <riquesouzalima11@gmail.com>
2020-05-17 03:06:35 -0300

    Movement

---
+++ SoccerStory/Assets/Script/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+
+
+}
